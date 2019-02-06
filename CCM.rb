1d2d1472da1cb285de84a2d0e235b03a292ced5e
Ben <43816668+reycarr@users.noreply.github.com>
2019-02-06 17:45:59 +0000

    Picker is now useable for board input. Solve isn't returning every combo

--- CCM.rb
+++ CCM.rb
@@ -5,29 +5,28 @@ def pick_vowel(x)
   vowels = { A: 15, E: 21, I: 13, O: 13, U: 5 }
   v_choice = []
   vowels.map {|letter, number| number.times {v_choice << letter}}
-  v_choice.shuffle!.pop(x)
+  v_choice.shuffle![0,x].join
 end
 
 def pick_consonant (y)
   consonants = {B: 2,  C: 3, D: 6, F: 2, G: 3, H: 2, J: 1, K: 1, L: 5, M: 4, N: 8, P: 4, Q: 1, R: 9, S: 9, T: 9, V: 1, W: 1, X: 1, Y: 1, Z: 1}
   c_choice = []
   consonants.map {|letter, number| number.times {c_choice << letter}}
-  c_choice.shuffle!.pop(y)
+  c_choice.shuffle![0,y].join
 end
+
+def solve(board)
+  puts (3..9).flat_map{|size| board.split(//).combination(size).map(&:join)}
+end
+
 
 num_of_vowels = [3,4,5].sample
 num_of_consonants = 9 - num_of_vowels
 
-pick_consonant(num_of_consonants)
-print pick_vowel(num_of_vowels)
-
-
-=begin
-def solver(board)
-  puts (2..9).flat_map{|size| board.combination(size).map(&:join)}
-end
-
-solver(board)
-=end
+board = "#{pick_consonant(num_of_consonants)}#{pick_vowel(num_of_vowels)}"
+puts board.split.shuffle!
+puts " "
+puts "Possible outcomes for this board are:"
+solve(board)
 
 #crossref solver output with dictionary
--- vowelgen.rb
+++ vowelgen.rb
@@ -2,7 +2,7 @@ def pick_vowel(x)
   vowels = { A: 15, E: 21, I: 13, O: 13, U: 5 }
   v_choice = []
   vowels.map {|letter, number| number.times {v_choice << letter}}
-  x.times {print v_choice.shuffle!.pop(1)}.values
+  x.times {print v_choice.shuffle!.pop(1)}
 end
 
 pick_vowel(5)
