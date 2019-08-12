d1f077261ce8decfb331a229c6fd44be14f6eaf4
Ben Reynolds-Carr <bjr.kitz@gmail.com>
2019-08-12 17:25:40 +0100

    Refactored to remove duplication
    * guard statement removed to separate guard function called at start of function
    * Neatened up with semicolons

--- JavaScript/src/Countdown.js
+++ JavaScript/src/Countdown.js
@@ -1,10 +1,10 @@
 function Countdown() {
-  this.board = []
+  this.board = [];
   this.consonants = ["B", "C", "D", "F", "G", "H", "J", "K", "L",
                      "M", "N", "P", "Q", "R", "S", "T", "V", "W",
                      "X", "Y", "Z"
-                    ]
-  this.vowels = ["A", "E", "I", "O", "U"]
+                   ];
+  this.vowels = ["A", "E", "I", "O", "U"];
 }
 
 Countdown.prototype.showBoard = function() {
@@ -12,24 +12,32 @@ Countdown.prototype.showBoard = function() {
 }
 
 Countdown.prototype.getLetter = function(type) {
-  var selection = this.consonants
-  if (type === "vowel") { selection = this.vowels }
+  var selection = this.consonants;
+  if (type === "vowel") {
+    selection = this.vowels;
+  }
 
-  return selection[Math.floor(Math.random()*selection.length)]
+  return selection[Math.floor(Math.random()*selection.length)];
+}
+
+Countdown.prototype.boardLengthCheck = function(type) {
+  if (this.board.length >= 9) {
+    throw 'The board can only be 9 letters!';
+  }
 }
 
 Countdown.prototype.consonant = function() {
-  if (this.board.length >= 9) { throw 'The board can only be 9 letters!' }
+  this.boardLengthCheck();
 
-  var letter = this.getLetter("consonant")
-  this.board.push(letter)
-  return letter
+  var letter = this.getLetter("consonant");
+  this.board.push(letter);
+  return letter;
 }
 
 Countdown.prototype.vowel = function() {
-  if (this.board.length >= 9) { throw 'The board can only be 9 letters!' }
+  this.boardLengthCheck();
 
-  var letter = this.getLetter("vowel")
-  this.board.push(letter)
-  return letter
+  var letter = this.getLetter("vowel");
+  this.board.push(letter);
+  return letter;
 }
