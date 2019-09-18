12a78909d0e2b46885082046b6e05a8614d55bac
Ben Reynolds-Carr <bjr.kitz@gmail.com>
2019-09-18 20:39:24 +0100

    Added constants to Countdown class to extract magic numbers
    * Constants are also interpolated in error messages for easier changing later

--- JavaScript/src/Countdown.js
+++ JavaScript/src/Countdown.js
@@ -1,7 +1,12 @@
+const EMPTYBOARD = 0
+const MAXBOARDLENGTH = 9
+const MAXCONSONANTS = 6
+const MAXVOWELS = 5
+
 function Countdown() {
   this.board = [];
-  this.numberOfConsonants = 0
-  this.numberOfVowels = 0
+  this.numberOfConsonants = EMPTYBOARD
+  this.numberOfVowels = EMPTYBOARD
   this.consonants = ["B", "C", "D", "F", "G", "H", "J", "K", "L",
                      "M", "N", "P", "Q", "R", "S", "T", "V", "W",
                      "X", "Y", "Z"
@@ -25,16 +30,16 @@ Countdown.prototype.getLetter = function(type) {
 }
 
 Countdown.prototype.boardLengthCheck = function() {
-  if (this.board.length >= 9) {
-    throw 'The board can only be 9 letters!';
+  if (this.board.length >= MAXBOARDLENGTH) {
+    throw `The board can only be ${MAXBOARDLENGTH} letters!`;
   }
 }
 
 Countdown.prototype.consonant = function() {
   this.boardLengthCheck();
 
-  if (this.numberOfConsonants >= 6) {
-    throw 'The board can only have 6 consonants';
+  if (this.numberOfConsonants >= MAXCONSONANTS) {
+    throw `The board can only have ${MAXCONSONANTS} consonants`;
   }
   
   var letter = this.getLetter("consonant");
@@ -45,8 +50,8 @@ Countdown.prototype.consonant = function() {
 Countdown.prototype.vowel = function() {
   this.boardLengthCheck();
 
-  if (this.numberOfVowels >= 5) {
-    throw 'The board can only have 5 vowels';
+  if (this.numberOfVowels >= MAXVOWELS) {
+    throw `The board can only have ${MAXVOWELS} vowels`;
   }
 
   var letter = this.getLetter("vowel");
